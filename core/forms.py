d8aa7d9e1037e47905c1b14c89f3d37641cf484b
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2020-06-13 09:13:55 +0100

    create contact form

--- core/forms.py
+++ core/forms.py
@@ -1,3 +1,23 @@
 from django import forms
-from .models import Message
+from .models import ContactMessage
+
+class ContactMessageForm(forms.ModelForm):
+    class Meta:
+        model = ContactMessage
+        fields = [
+            'first_name',
+            'last_name',
+            'email_address',
+            'phone_number',
+            'message_text',
+        ]
+
+        fields = {
+            'first_name': "Enter your name:",
+            'email_address': "Enter your e-mail address:",
+            'phone_number': "Enter your phone number:",
+            'message_text': "Write your message below:",
+        }
+
+        
 
