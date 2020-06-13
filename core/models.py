991cc2eec6d343d59ce8a1ec167d5aa1e049cbb1
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2020-06-13 09:01:27 +0100

    create ContactMessage model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ContactMessage(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email_address = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    message_text = models.TextField()
+
+    def __str__(self):
+        return self.email_address
