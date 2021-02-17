9bf5bf67d53e304a63926d0091a6fd18c04fcc63
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-02-17 11:24:28 +0000

    Views tests added

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,70 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from django.urls import reverse
 
-# Create your tests here.
+
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+class TestHomeView(TestCase):
+
+    def test_view_url_exists_at_desired_location(self) -> None:
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self) -> None:
+        response = self.client.get(reverse('core:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self) -> None:
+        response = self.client.get(reverse('core:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+class TestHistoryView(TestCase):
+
+    def test_view_url_exists_at_desired_location(self) -> None:
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self) -> None:
+        response = self.client.get(reverse('core:history'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self) -> None:
+        response = self.client.get(reverse('core:history'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'history.html')
+
+
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+class TestContactView(TestCase):
+
+    def test_view_url_exists_at_desired_location(self) -> None:
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self) -> None:
+        response = self.client.get(reverse('core:contact'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self) -> None:
+        response = self.client.get(reverse('core:contact'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact.html')
+
+
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+class TestMapView(TestCase):
+
+    def test_view_url_exists_at_desired_location(self) -> None:
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self) -> None:
+        response = self.client.get(reverse('core:map'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self) -> None:
+        response = self.client.get(reverse('core:map'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'map.html')
