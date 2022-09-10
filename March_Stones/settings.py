87ff6d1c1aae18f47ed3fbb92b6a3e1323c39145
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2022-09-10 21:54:23 +0200

    Add allowed hosts.

--- March_Stones/settings.py
+++ March_Stones/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['139.162.166.36']
 
 GOOGLE_MAP_API_KEY = os.environ.get('GOOGLE_MAP_API_KEY')
 
